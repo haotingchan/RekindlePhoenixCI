a9d85d6c6136e8904ba6a99d56e7252cb39cbff1
原燊 丁 <david@kjsoft.com.tw>
2019-03-22 14:13:43 +0800

    [add] P0030 / P0050

--- DataObjects/Dao/Together/SpecificDao/DP0030.cs
+++ DataObjects/Dao/Together/SpecificDao/DP0030.cs
@@ -1,19 +1,18 @@
 ﻿using BusinessObjects;
 using OnePiece;
-using Oracle.ManagedDataAccess.Client;
-using System;
 using System.Collections.Generic;
 using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DataObjects.Dao.Together.SpecificDao {
-   public class DP0030 : DataGate {
+    public class DP0030 {
 
       public DataTable ExecuteStoredProcedure(string IN_START_DATE , string IN_END_DATE , string IN_SYSTEM ,
-         string IN_GROUP_TYPE) {
-         List<DbParameterEx> parms = new List<DbParameterEx>() {
+         string IN_GROUP_TYPE,string posConn) {
+
+            //切換DB Connection
+            Db posDB = new Db(posConn, "Oracle.ManagedDataAccess.Client", "");
+
+            List<DbParameterEx> parms = new List<DbParameterEx>() {
             new DbParameterEx("IN_START_DATE",IN_START_DATE),
             new DbParameterEx("IN_END_DATE",IN_END_DATE),
             new DbParameterEx("IN_SYSTEM",IN_SYSTEM),
@@ -23,7 +22,7 @@ namespace DataObjects.Dao.Together.SpecificDao {
 
          string sql = "pos_owner.PKG_UTILITY.SP_QUERY_SEARCH_TIMES";
 
-         return db.ExecuteStoredProcedureEx(sql , parms , true);
+         return posDB.ExecuteStoredProcedureEx(sql , parms , true);
       }
 
    }
