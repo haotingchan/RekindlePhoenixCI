8553703e7888ca0a972ffc377b039956e275c2fc
原燊 丁 <david@kjsoft.com.tw>
2019-03-29 18:57:59 +0800

    [Add]50010 [Modify]AMPD / APDK

---
+++ DataObjects/Dao/Together/SpecificDao/D50010.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Data;
+
+namespace DataObjects.Dao.Together.SpecificDao {
+   public class D50010 : DataGate
+    {
+      public DataTable GetData(string sortType, DateTime adtdate, string paramKey, string kindId2, string kindId, string prodId, string fcmRange) {
+         object[] parms = {
+                ":as_sort_type", sortType,
+                ":adt_date", adtdate,
+                ":as_ammd_param_key",paramKey,
+                ":as_ammd_kind_id2",kindId2,
+                ":as_ammd_kind_id",kindId,
+                ":as_ammd_prod_id",prodId
+            };
+
+         string sql = string.Format(@"  SELECT AMMD_BRK_NO, 
+            AMMD_ACC_NO, BRK_ABBR_NAME, 
+        TRIM(AMMD_PROD_ID) as AMMD_PROD_ID,   
+         AMMD_W_TIME,   
+         AMMD_R_TIME,
+         r_second,
+         m_second, 
+         cp_valid,
+         AMMD_KEEP_TIME,
+         AMMD_WEIGHT_P,
+         AMMD_WEIGHT_Q,
+         AMMD_WEIGHT_KIND,
+         AMMD_RESULT,
+         AMMD_OQ_CODE from (
+ SELECT  case when :as_sort_type ='F' then ammd_brk_no || ammd_acc_no else ammd_prod_type || ammd_prod_id end as  cp_group1,  
+         case when :as_sort_type ='F' then  ammd_prod_type || ammd_prod_id else ammd_brk_no|| ammd_acc_no end as cp_group2 ,
+         ammd_brk_no,
+         AMMD_ACC_NO,
+        (SELECT NVL(ABRK_NAME,'') FROM ci.ABRK
+          WHERE ABRK_NO = AMMD_BRK_NO ) as BRK_ABBR_NAME,  
+         AMMD_PROD_ID,   
+         AMMD_W_TIME,   
+         AMMD_R_TIME, 
+         AMMD_M_TIME, 
+         AMMD_PROD_TYPE,
+         case when AMMD_R_TIME >= AMMD_W_TIME then 0 else round(fut.TIME_DIFF(AMMD_R_TIME,AMMD_W_TIME),0) end   as r_second,
+         round(fut.TIME_DIFF(AMMD_W_TIME,AMMD_M_TIME),0)   as m_second,
+         case when AMMD_VALID_CNT > 0 then 'Y' else '' end as cp_valid,
+         AMMD_KEEP_TIME,
+         AMMD_WEIGHT_P,
+         AMMD_WEIGHT_Q,
+         AMMD_WEIGHT_KIND,
+         AMMD_RESULT,
+         AMMD_OQ_CODE,
+         AMMD_Q_NO  
+    FROM ci.AMMD  
+   WHERE AMMD_DATE = :adt_date  AND  
+         AMMD_DATA_TYPE  IN ('Q'  ,'q')
+         and ammd_PARAM_KEY like :as_ammd_PARAM_KEY
+         and ammd_kind_id2 like  :as_ammd_kind_id2
+         and ammd_KIND_ID like :as_ammd_KIND_ID
+         --and ammd_PROD_ID like :as_ammd_PROD_ID
+         order by cp_group1 , cp_group2 , ammd_w_time)", fcmRange);
+
+         return db.GetDataTable(sql, parms);
+      }
+
+      public DataTable GetExportData(string sortType, DateTime adtdate, string paramKey, string kindId2, string kindId, string prodId, string fcmRange) {
+         object[] parms = {
+                ":as_sort_type", sortType,
+                ":adt_date", adtdate,
+                ":as_ammd_param_key",paramKey,
+                ":as_ammd_kind_id2",kindId2,
+                ":as_ammd_kind_id",kindId,
+                ":as_ammd_prod_id",prodId
+            };
+
+         string sql = string.Format(@"  SELECT AMMD_BRK_NO, 
+            AMMD_ACC_NO, BRK_ABBR_NAME, 
+          AMMD_PROD_TYPE,
+        TRIM(AMMD_PROD_ID) as AMMD_PROD_ID,   
+         AMMD_W_TIME,   
+         AMMD_R_TIME, 
+         AMMD_M_TIME, 
+         r_second,
+         m_second, 
+         cp_valid,
+         AMMD_KEEP_TIME,
+         AMMD_WEIGHT_P,
+         AMMD_WEIGHT_Q,
+         AMMD_WEIGHT_KIND,
+         AMMD_RESULT,
+         AMMD_OQ_CODE,
+         AMMD_Q_NO from (
+ SELECT  case when :as_sort_type ='F' then ammd_brk_no || ammd_acc_no else ammd_prod_type || ammd_prod_id end as  cp_group1,  
+         case when :as_sort_type ='F' then  ammd_prod_type || ammd_prod_id else ammd_brk_no|| ammd_acc_no end as cp_group2 ,
+         ammd_brk_no,
+         AMMD_ACC_NO,
+        (SELECT NVL(ABRK_NAME,'') FROM ci.ABRK
+          WHERE ABRK_NO = AMMD_BRK_NO ) as BRK_ABBR_NAME,  
+         AMMD_PROD_ID,   
+         AMMD_W_TIME,   
+         AMMD_R_TIME, 
+         AMMD_M_TIME, 
+         AMMD_PROD_TYPE,
+         case when AMMD_R_TIME >= AMMD_W_TIME then 0 else round(fut.TIME_DIFF(AMMD_R_TIME,AMMD_W_TIME),0) end   as r_second,
+         round(fut.TIME_DIFF(AMMD_W_TIME,AMMD_M_TIME),0)   as m_second,
+         case when AMMD_VALID_CNT > 0 then 'Y' else '' end as cp_valid,
+         AMMD_KEEP_TIME,
+         AMMD_WEIGHT_P,
+         AMMD_WEIGHT_Q,
+         AMMD_WEIGHT_KIND,
+         AMMD_RESULT,
+         AMMD_OQ_CODE,
+         AMMD_Q_NO  
+    FROM ci.AMMD  
+   WHERE AMMD_DATE = :adt_date  AND  
+         AMMD_DATA_TYPE  IN ('Q'  ,'q')
+         and ammd_PARAM_KEY like :as_ammd_PARAM_KEY
+         and ammd_kind_id2 like  :as_ammd_kind_id2
+         and ammd_KIND_ID like :as_ammd_KIND_ID
+         and ammd_PROD_ID like :as_ammd_PROD_ID
+         order by cp_group1 , cp_group2 , ammd_w_time)", fcmRange);
+
+         return db.GetDataTable(sql, parms);
+      }
+
+   }
+}
---
+++ PhoenixCI/FormUI/Prefix5/W50010.cs
@@ -0,0 +1,137 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using DataObjects.Dao.Together.SpecificDao;
+using BusinessObjects.Enums;
+using DevExpress.Spreadsheet;
+using BusinessObjects;
+using Common;
+using BaseGround.Report;
+using BaseGround;
+using System.Data.Common;
+using static BaseGround.Report.ReportHelper;
+using System.Windows.Forms;
+using PhoenixCI.Report;
+using DevExpress.XtraPrinting.Caching;
+using System;
+using DataObjects.Dao.Together;
+using BaseGround.Shared;
+using System.Linq;
+using System.IO;
+using DevExpress.XtraEditors.Controls;
+
+namespace PhoenixCI.FormUI.Prefix5
+{
+   public partial class W50010 : FormParent
+   {
+      private D50010 dao50010;
+
+      protected class LookupItem {
+         public string ValueMember { get; set; }
+         public string DisplayMember { get; set; }
+      }
+
+      public W50010(string programID, string programName) : base(programID, programName)
+      {
+         InitializeComponent();
+         this.Text = _ProgramID + "─" + _ProgramName;
+         dao50010 = new D50010();
+         TxtDate.DateTimeValue = GlobalInfo.OCF_DATE;
+         APDK daoAPDK = new APDK();
+
+         //設定下拉選單
+         Fcm_SNo.SetDataTable(new AMPD().ListByFcmAccNo(), "AMPD_FCM_NO", "cp_display", TextEditStyles.DisableTextEditor, "");
+         Fcm_SNo.EditValue = "";
+         Fcm_ENo.SetDataTable(new AMPD().ListByFcmAccNo(), "AMPD_FCM_NO", "cp_display", TextEditStyles.DisableTextEditor, "");
+         Fcm_ENo.EditValue = "";
+         Prod_ct.SetDataTable(daoAPDK.dw_prod_500xx("APDK_PROD_TYPE,APDK_PARAM_KEY", "'%','%'"), "APDK_PARAM_KEY", "APDK_PARAM_KEY",
+            TextEditStyles.DisableTextEditor, null);
+         Prod_ct.EditValue = "%";
+         Kind_id_st.SetDataTable(daoAPDK.dw_prod_500xx("APDK_KIND_ID_STO", "'%'"), "APDK_KIND_ID_STO", "APDK_KIND_ID_STO",
+            TextEditStyles.DisableTextEditor, null);
+         Kind_id_st.EditValue = "%";
+         Kind_id_O.SetDataTable(daoAPDK.dw_prod_500xx("APDK_PROD_TYPE,APDK_KIND_ID", "'%','%'"), "APDK_KIND_ID", "APDK_KIND_ID",
+            TextEditStyles.DisableTextEditor, null);
+         Kind_id_O.EditValue = "%";
+
+
+         //排序方式 下拉選單
+         List<LookupItem> markret = new List<LookupItem>(){
+                                        new LookupItem() { ValueMember = "O", DisplayMember = "一般"},
+                                        new LookupItem() { ValueMember = "N", DisplayMember = "盤後" }};
+         Extension.SetDataTable(MarketTime, markret, "ValueMember", "DisplayMember", TextEditStyles.DisableTextEditor, null);
+         MarketTime.EditValue = "O";
+         List<LookupItem> printSort = new List<LookupItem>(){
+                                        new LookupItem() { ValueMember = "M", DisplayMember = "造市者"},
+                                        new LookupItem() { ValueMember = "P", DisplayMember = "商品" }};
+         Extension.SetDataTable(PrintSort, printSort, "ValueMember", "DisplayMember", TextEditStyles.DisableTextEditor, null);
+         PrintSort.EditValue = "M";
+
+      }
+
+      protected override ResultStatus Retrieve()
+      {
+         string paramKey = "%" + Prod_ct.EditValue.AsString() + "%";
+         string kindIdSt = "%" + Kind_id_st.EditValue.AsString() + "%";
+         string kindIdO = "%" + Kind_id_O.EditValue.AsString() + "%";
+         string[] showColCaption = { "期貨商", "期貨商名稱", "帳號","商品名稱", "報價時間", "最接近報價詢價時間",
+            "尋報價時間差(秒)", "報價維持時間(秒)", "報價有效", "計入維持時間(秒)", "價差權數", "數量權數", "標的權數","績效","報價單別" };
+
+         DataTable dt = new DataTable();
+         dt = dao50010.GetData("F", TxtDate.DateTimeValue, paramKey, kindIdSt, kindIdO, "%", "");
+         gcMain.DataSource = dt;
+
+         //設定 column caption
+         foreach (DataColumn dc in dt.Columns) {
+            gvMain.SetColumnCaption(dc.ColumnName, showColCaption[dt.Columns.IndexOf(dc)]);
+            gvMain.Columns[dc.ColumnName].OptionsColumn.AllowEdit = false;
+         }
+         gvMain.BestFitColumns();
+         return ResultStatus.Success;
+      }
+
+      protected override ResultStatus Export()
+      {
+         string paramKey = "%" + Prod_ct.EditValue.AsString() + "%";
+         string kindIdSt = "%" + Kind_id_st.EditValue.AsString() + "%";
+         string kindIdO = "%" + Kind_id_O.EditValue.AsString() + "%";
+         string fileName = string.Format("{0}_{1}.csv", _ProgramID, DateTime.Now.ToString("yyyy.MM.dd-HH.mm.ss"));
+         string filepath= Path.Combine(GlobalInfo.DEFAULT_REPORT_DIRECTORY_PATH, fileName);
+
+         string[] exportColCaption = { "期貨商", "帳號", "期貨商名稱","期貨/選擇權","商品", "報價時間", "最接近報價詢價時間",
+            "最小成交回報檔時間", "r_second","m_second", "有效報價", "計入維持時間(秒)", "價差權數", "數量權數", "標的權數","績效","報價單別" ,"單號"};
+
+         DataTable dtExport = new DataTable();
+         dtExport = dao50010.GetExportData("F", TxtDate.DateTimeValue, "%", "%", "%", "%", "");
+         gcExport.DataSource = dtExport;
+
+         foreach (DataColumn dc in dtExport.Columns) {
+            gvExport.SetColumnCaption(dc.ColumnName, exportColCaption[dtExport.Columns.IndexOf(dc)]);
+         }
+
+         gvExport.Columns["AMMD_RESULT"].DisplayFormat.FormatString = "n2";
+         gvExport.Columns["AMMD_W_TIME"].DisplayFormat.FormatString = "yyyy/MM/dd HH:mm";
+         gvExport.Columns["AMMD_R_TIME"].DisplayFormat.FormatString = "yyyy/MM/dd HH:mm";
+         gvExport.Columns["AMMD_M_TIME"].DisplayFormat.FormatString = "yyyy/MM/dd HH:mm";
+
+         gvExport.ExportToCsv(filepath);
+
+         return ResultStatus.Success;
+      }
+
+      protected override ResultStatus Print(ReportHelper reportHelper)
+      {
+         base.Print(reportHelper);
+         return ResultStatus.Success;
+      }
+
+      protected override ResultStatus ActivatedForm() {
+         base.ActivatedForm();
+
+         _ToolBtnRetrieve.Enabled = true;
+         _ToolBtnExport.Enabled = true;
+         _ToolBtnPrintAll.Enabled = true;
+
+         return ResultStatus.Success;
+      }
+   }
+}
