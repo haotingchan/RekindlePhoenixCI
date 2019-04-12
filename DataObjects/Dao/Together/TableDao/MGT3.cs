c69787c968ca818cf6eacb78e065c9c3a6d28c8d
winni <winni@kjsoft.com.tw>
2019-04-12 21:11:21 +0800

    [Add] W49050 / D49050 / MGT3

---
+++ DataObjects/Dao/Together/TableDao/MGT3.cs
@@ -0,0 +1,29 @@
+﻿using BusinessObjects;
+using System.Data;
+/// <summary>
+/// Winni, 2019/4/12
+/// </summary>
+namespace DataObjects.Dao.Together.TableDao {
+   public class MGT3 : DataGate {
+
+      /// <summary>
+      /// save ci.mgt3
+      /// </summary>
+      /// <param name="inputData"></param>
+      /// <returns></returns>
+      public ResultData UpdateData(DataTable inputData) {
+
+         string sql = @"
+select
+    mgt3_date_fm,
+    mgt3_date_to, 
+    mgt3_memo, 
+    mgt3_w_time, 
+    mgt3_w_user_id
+from ci.mgt3
+";
+
+         return db.UpdateOracleDB(inputData , sql);
+      }
+   }
+}
