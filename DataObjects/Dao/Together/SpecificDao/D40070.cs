607f21cf78b187da34fe06a6c3ac40565fc39568
Lukas <lukas@kjsoft.com.tw>
2019-04-17 18:10:06 +0800

    [Add] D40070/ W40070 [Modify] 50070 template/ W42011 by Lukas

---
+++ DataObjects/Dao/Together/SpecificDao/D40070.cs
@@ -0,0 +1,29 @@
+﻿using BusinessObjects;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// Lukas, 2019/4/17
+/// </summary>
+namespace DataObjects.Dao.Together.SpecificDao {
+    public class D40070: DataGate {
+
+        public DataTable d_40070_scrn(string as_ymd, string as_cond) {
+
+            List<DbParameterEx> parms = new List<DbParameterEx>() {
+            new DbParameterEx("as_ymd",as_ymd),
+            new DbParameterEx("as_cond",as_cond)
+            //new DbParameterEx("RETURNPARAMETER",0)
+         };
+
+            string sql = "CI.SP_H_TXN_40070_SCRN";
+
+            return db.ExecuteStoredProcedureEx(sql, parms, true);
+        }
+
+    }
+}
---
+++ PhoenixCI/FormUI/Prefix4/W40070.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+using BaseGround;
+using DataObjects.Dao.Together.SpecificDao;
+using BusinessObjects.Enums;
+using BaseGround.Shared;
+using DevExpress.XtraEditors.Controls;
+using Common;
+
+/// <summary>
+/// Lukas, 2019/4/17
+/// </summary>
+namespace PhoenixCI.FormUI.Prefix4 {
+    /// <summary>
+    /// 40070
+    /// </summary>
+    public partial class W40070 : FormParent {
+
+        private D40070 dao40070;
+
+        public W40070(string programID, string programName) : base(programID, programName) {
+            InitializeComponent();
+            this.Text = _ProgramID + "─" + _ProgramName;
+        }
+
+        protected override ResultStatus Open() {
+            base.Open();
+            //日期
+            txtSDate.DateTimeValue = DateTime.Now;
+
+            //設定調整商品條件下拉選單
+            List<LookupItem> modelType = new List<LookupItem>(){
+                                        new LookupItem() { ValueMember = "S", DisplayMember = "SMA達調整標準"},
+                                        new LookupItem() { ValueMember = "a", DisplayMember = "任一model達調整標準" },
+                                        new LookupItem() { ValueMember = "%", DisplayMember = "全部商品" }};
+            Extension.SetDataTable(ddlModel, modelType, "ValueMember", "DisplayMember", TextEditStyles.DisableTextEditor, "");
+
+#if DEBUG
+            txtSDate.EditValue = "2018/10/11";
+#endif
+
+            return ResultStatus.Success;
+        }
+
+        protected override ResultStatus ActivatedForm() {
+            base.ActivatedForm();
+
+            _ToolBtnInsert.Enabled = false;//當按下此按鈕時,Grid新增一筆空的(還未存檔都是暫時的)
+            _ToolBtnSave.Enabled = true;//儲存(把新增/刪除/修改)多筆的結果一次更新到資料庫
+            _ToolBtnDel.Enabled = false;//先選定刪除grid上面的其中一筆,然後按下此刪除按鈕(還未存檔都是暫時的)
+
+            _ToolBtnRetrieve.Enabled = true;//畫面查詢條件選定之後,按下此按鈕,讀取資料 to Grid
+            _ToolBtnRun.Enabled = false;//執行,跑job專用按鈕
+
+            _ToolBtnImport.Enabled = false;//匯入
+            _ToolBtnExport.Enabled = false;//匯出,格式可以為 pdf/xls/txt/csv, 看功能
+            _ToolBtnPrintAll.Enabled = true;//列印
+
+            return ResultStatus.Success;
+        }
+
+        protected override ResultStatus Retrieve() {
+
+            try {
+                dao40070 = new D40070();
+                DataTable dt40070 = dao40070.d_40070_scrn(txtSDate.DateTimeValue.ToString("yyyyMMdd"),ddlModel.EditValue.AsString());
+                gcMain.DataSource = dt40070;
+                //複製
+                //dw_1.RowsCopy(1, dw_1.rowcount(), primary!, ids_tmp, 1, primary!)
+
+                //dw_1.setfilter(" ab_type in ('-','A')")
+                //dw_1.filter()
+            }
+            catch (Exception ex) {
+                MessageDisplay.Error("讀取錯誤");
+                throw ex;
+            }
+
+            return ResultStatus.Success;
+        }
+
+    }
+}
