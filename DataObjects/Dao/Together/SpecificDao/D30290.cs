0e0fb5908df1ca2e856e0d3abd984669aefdbdc5
john <john@kjsoft.com.tw>
2019-04-23 16:14:27 +0800

    [add]30290

--- DataObjects/Dao/Together/SpecificDao/D30290.cs
+++ DataObjects/Dao/Together/SpecificDao/D30290.cs
@@ -10,12 +10,48 @@ using System.Threading.Tasks;
 /// </summary>
 namespace DataObjects.Dao.Together.SpecificDao {
     public class D30290:DataGate {
+
+      /// <summary>
+      /// dw_ymd
+      /// </summary>
+      /// <param name="as_symd"></param>
+      /// <returns>YMD</returns>
+      public DataTable ListEffectiveYmd(string as_ymd)
+      {
+         object[] parms = {
+                ":as_ymd", as_ymd
+            };
+
+         string sql =
+                  @"
+                     SELECT DISTINCT YMD FROM 
+                     (
+                         SELECT PL2_EFFECTIVE_YMD AS YMD
+                         FROM ci.PL2
+                         WHERE PL2_YMD >= :as_ymd
+                         GROUP BY PL2_EFFECTIVE_YMD
+                         UNION ALL
+                         SELECT PLS2_EFFECTIVE_YMD
+                         FROM ci.PLS2
+                         WHERE PLS2_YMD >= :as_ymd
+                         GROUP BY PLS2_EFFECTIVE_YMD
+                         UNION ALL
+                         SELECT '' FROM DUAL
+                     )
+                     ORDER BY YMD
+                     ";
+         DataTable dtResult = db.GetDataTable(sql, parms);
+
+         return dtResult;
+      }
+
+
       /// <summary>
       /// d_30290
       /// </summary>
       /// <param name="as_symd"></param>
       /// <returns></returns>
-        public DataTable GetData(string as_symd) {
+      public DataTable GetData(string as_symd) {
 
             object[] parms = {
                 ":as_symd", as_symd
---
+++ PhoenixCI/BusinessLogic/Prefix3/B30290.cs
@@ -0,0 +1,75 @@
+﻿using Common;
+using DataObjects.Dao.Together.SpecificDao;
+using System;
+using System.Data;
+using DevExpress.Spreadsheet;
+using BaseGround.Shared;
+using System.IO;
+using DevExpress.XtraEditors;
+/// <summary>
+/// john,20190329,每月報局交易量報表(國內期貨暨選擇權)
+/// </summary>
+namespace PhoenixCI.BusinessLogic.Prefix3
+{
+   /// <summary>
+   /// 每月報局交易量報表(國內期貨暨選擇權)
+   /// </summary>
+   public class B30290
+   {
+      private readonly string _lsFile;
+      private readonly string _emDateText;
+      private readonly string _lsMarketCode;
+      private readonly DateTime _emEndDateText;
+      private D30290 dao30290;
+
+      public B30290()
+      {
+         dao30290 = new D30290();
+      }
+
+      public B30290(string lsFile, string emDate,string MarketCode, DateTime emEndDate)
+      {
+         _lsFile = lsFile;
+         _emDateText = emDate;
+         _lsMarketCode = MarketCode;
+         _emEndDateText = emEndDate;
+         dao30290 = new D30290();
+      }
+
+      public DataTable GetEffectiveYMD(string emDateText)
+      {
+         return dao30290.ListEffectiveYmd(emDateText.Replace("/",""));
+      }
+
+      /// <summary>
+      /// //取得前一季月份
+      /// </summary>
+      /// <returns></returns>
+      public string LastQuarter(string emDateText)
+      {
+
+         return "";
+      }
+
+      /// <summary>
+      /// wf_30780_1()
+      /// </summary>
+      /// <returns></returns>
+      public string WF30780one()
+      {
+         try {
+
+            return MessageDisplay.MSG_OK;
+         }
+         catch (Exception ex) {
+            File.Delete(_lsFile);
+#if DEBUG
+            throw new Exception("WF30780one:" + ex.Message);
+#else
+            throw ex;
+#endif
+         }
+      }
+
+   }
+}
