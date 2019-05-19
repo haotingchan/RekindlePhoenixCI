d7f5bc96e05a31b1a90eb70660ae4c6b5e88e19b
winni <winni@kjsoft.com.tw>
2019-05-20 03:20:40 +0800

    [Modify] 28110 / 55050

--- PhoenixCI/FormUI/Prefix5/W55050.designer.cs
+++ PhoenixCI/FormUI/Prefix5/W55050.designer.cs
@@ -24,6 +24,8 @@
         /// </summary>
         private void InitializeComponent() {
          this.panFilter = new System.Windows.Forms.GroupBox();
+         this.txtEndMonth = new BaseGround.Widget.TextDateEdit();
+         this.txtStartMonth = new BaseGround.Widget.TextDateEdit();
          this.rgpType = new DevExpress.XtraEditors.RadioGroup();
          this.labFcmNo = new System.Windows.Forms.Label();
          this.label1 = new System.Windows.Forms.Label();
@@ -32,22 +34,22 @@
          this.cbxFcmStartNo = new DevExpress.XtraEditors.LookUpEdit();
          this.cbxFcmEndNo = new DevExpress.XtraEditors.LookUpEdit();
          this.labMsg = new System.Windows.Forms.Label();
-         this.txtStartMonth = new BaseGround.Widget.TextDateEdit();
-         this.txtEndMonth = new BaseGround.Widget.TextDateEdit();
+         this.r_frame = new DevExpress.XtraEditors.PanelControl();
          this.panParent.SuspendLayout();
          ((System.ComponentModel.ISupportInitialize)(this.ribbonControl)).BeginInit();
          this.panFilter.SuspendLayout();
+         ((System.ComponentModel.ISupportInitialize)(this.txtEndMonth.Properties)).BeginInit();
+         ((System.ComponentModel.ISupportInitialize)(this.txtStartMonth.Properties)).BeginInit();
          ((System.ComponentModel.ISupportInitialize)(this.rgpType.Properties)).BeginInit();
          ((System.ComponentModel.ISupportInitialize)(this.cbxFcmStartNo.Properties)).BeginInit();
          ((System.ComponentModel.ISupportInitialize)(this.cbxFcmEndNo.Properties)).BeginInit();
-         ((System.ComponentModel.ISupportInitialize)(this.txtStartMonth.Properties)).BeginInit();
-         ((System.ComponentModel.ISupportInitialize)(this.txtEndMonth.Properties)).BeginInit();
+         ((System.ComponentModel.ISupportInitialize)(this.r_frame)).BeginInit();
+         this.r_frame.SuspendLayout();
          this.SuspendLayout();
          // 
          // panParent
          // 
-         this.panParent.Controls.Add(this.labMsg);
-         this.panParent.Controls.Add(this.panFilter);
+         this.panParent.Controls.Add(this.r_frame);
          this.panParent.Size = new System.Drawing.Size(1034, 581);
          // 
          // ribbonControl
@@ -67,109 +69,14 @@
          this.panFilter.Controls.Add(this.labDate);
          this.panFilter.Controls.Add(this.cbxFcmStartNo);
          this.panFilter.Controls.Add(this.cbxFcmEndNo);
-         this.panFilter.Font = new System.Drawing.Font("新細明體", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-         this.panFilter.Location = new System.Drawing.Point(36, 33);
+         this.panFilter.Font = new System.Drawing.Font("新細明體", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+         this.panFilter.ForeColor = System.Drawing.Color.Navy;
+         this.panFilter.Location = new System.Drawing.Point(16, 26);
          this.panFilter.Name = "panFilter";
-         this.panFilter.Size = new System.Drawing.Size(880, 216);
+         this.panFilter.Size = new System.Drawing.Size(788, 216);
          this.panFilter.TabIndex = 6;
          this.panFilter.TabStop = false;
          this.panFilter.Text = "請輸入交易日期";
-         // 
-         // rgpType
-         // 
-         this.rgpType.EditValue = "fcm";
-         this.rgpType.Location = new System.Drawing.Point(137, 122);
-         this.rgpType.Name = "rgpType";
-         this.rgpType.Properties.Items.AddRange(new DevExpress.XtraEditors.Controls.RadioGroupItem[] {
-            new DevExpress.XtraEditors.Controls.RadioGroupItem("fcm", "依期貨商別"),
-            new DevExpress.XtraEditors.Controls.RadioGroupItem("product", "依商品別")});
-         this.rgpType.Size = new System.Drawing.Size(283, 40);
-         this.rgpType.TabIndex = 4;
-         // 
-         // labFcmNo
-         // 
-         this.labFcmNo.AutoSize = true;
-         this.labFcmNo.Location = new System.Drawing.Point(64, 88);
-         this.labFcmNo.Name = "labFcmNo";
-         this.labFcmNo.Size = new System.Drawing.Size(67, 15);
-         this.labFcmNo.TabIndex = 16;
-         this.labFcmNo.Text = "期貨商：";
-         // 
-         // label1
-         // 
-         this.label1.AutoSize = true;
-         this.label1.Location = new System.Drawing.Point(399, 84);
-         this.label1.Name = "label1";
-         this.label1.Size = new System.Drawing.Size(15, 15);
-         this.label1.TabIndex = 14;
-         this.label1.Text = "~";
-         // 
-         // lbl1
-         // 
-         this.lbl1.AutoSize = true;
-         this.lbl1.Location = new System.Drawing.Point(243, 43);
-         this.lbl1.Name = "lbl1";
-         this.lbl1.Size = new System.Drawing.Size(15, 15);
-         this.lbl1.TabIndex = 11;
-         this.lbl1.Text = "~";
-         // 
-         // labDate
-         // 
-         this.labDate.AutoSize = true;
-         this.labDate.Location = new System.Drawing.Point(79, 47);
-         this.labDate.Name = "labDate";
-         this.labDate.Size = new System.Drawing.Size(52, 15);
-         this.labDate.TabIndex = 2;
-         this.labDate.Text = "月份：";
-         // 
-         // cbxFcmStartNo
-         // 
-         this.cbxFcmStartNo.Location = new System.Drawing.Point(137, 81);
-         this.cbxFcmStartNo.Name = "cbxFcmStartNo";
-         this.cbxFcmStartNo.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
-            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
-         this.cbxFcmStartNo.Size = new System.Drawing.Size(250, 26);
-         this.cbxFcmStartNo.TabIndex = 2;
-         // 
-         // cbxFcmEndNo
-         // 
-         this.cbxFcmEndNo.Location = new System.Drawing.Point(424, 81);
-         this.cbxFcmEndNo.Name = "cbxFcmEndNo";
-         this.cbxFcmEndNo.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
-            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
-         this.cbxFcmEndNo.Size = new System.Drawing.Size(250, 26);
-         this.cbxFcmEndNo.TabIndex = 3;
-         // 
-         // labMsg
-         // 
-         this.labMsg.AutoSize = true;
-         this.labMsg.ForeColor = System.Drawing.Color.Blue;
-         this.labMsg.Location = new System.Drawing.Point(32, 262);
-         this.labMsg.Name = "labMsg";
-         this.labMsg.Size = new System.Drawing.Size(169, 20);
-         this.labMsg.TabIndex = 18;
-         this.labMsg.Text = "訊息：資料轉出中........";
-         this.labMsg.Visible = false;
-         // 
-         // txtStartMonth
-         // 
-         this.txtStartMonth.DateTimeValue = new System.DateTime(2018, 12, 1, 0, 0, 0, 0);
-         this.txtStartMonth.DateType = BaseGround.Widget.TextDateEdit.DateTypeItem.Month;
-         this.txtStartMonth.EditValue = "2018/12";
-         this.txtStartMonth.EnterMoveNextControl = true;
-         this.txtStartMonth.Location = new System.Drawing.Point(137, 40);
-         this.txtStartMonth.MenuManager = this.ribbonControl;
-         this.txtStartMonth.Name = "txtStartMonth";
-         this.txtStartMonth.Properties.Appearance.Options.UseTextOptions = true;
-         this.txtStartMonth.Properties.Appearance.TextOptions.HAlignment = DevExpress.Utils.HorzAlignment.Center;
-         this.txtStartMonth.Properties.EditFormat.FormatString = "yyyyMM";
-         this.txtStartMonth.Properties.Mask.EditMask = "[1-9]\\d{3}/(0[1-9]|1[0-2])";
-         this.txtStartMonth.Properties.Mask.MaskType = DevExpress.XtraEditors.Mask.MaskType.RegEx;
-         this.txtStartMonth.Properties.Mask.ShowPlaceHolders = false;
-         this.txtStartMonth.Properties.Mask.UseMaskAsDisplayFormat = true;
-         this.txtStartMonth.Size = new System.Drawing.Size(100, 26);
-         this.txtStartMonth.TabIndex = 17;
-         this.txtStartMonth.TextMaskFormat = BaseGround.Widget.TextDateEdit.TextMaskFormatItem.IncludePrompt;
          // 
          // txtEndMonth
          // 
@@ -191,6 +98,118 @@
          this.txtEndMonth.TabIndex = 18;
          this.txtEndMonth.TextMaskFormat = BaseGround.Widget.TextDateEdit.TextMaskFormatItem.IncludePrompt;
          // 
+         // txtStartMonth
+         // 
+         this.txtStartMonth.DateTimeValue = new System.DateTime(2018, 12, 1, 0, 0, 0, 0);
+         this.txtStartMonth.DateType = BaseGround.Widget.TextDateEdit.DateTypeItem.Month;
+         this.txtStartMonth.EditValue = "2018/12";
+         this.txtStartMonth.EnterMoveNextControl = true;
+         this.txtStartMonth.Location = new System.Drawing.Point(137, 40);
+         this.txtStartMonth.MenuManager = this.ribbonControl;
+         this.txtStartMonth.Name = "txtStartMonth";
+         this.txtStartMonth.Properties.Appearance.Options.UseTextOptions = true;
+         this.txtStartMonth.Properties.Appearance.TextOptions.HAlignment = DevExpress.Utils.HorzAlignment.Center;
+         this.txtStartMonth.Properties.EditFormat.FormatString = "yyyyMM";
+         this.txtStartMonth.Properties.Mask.EditMask = "[1-9]\\d{3}/(0[1-9]|1[0-2])";
+         this.txtStartMonth.Properties.Mask.MaskType = DevExpress.XtraEditors.Mask.MaskType.RegEx;
+         this.txtStartMonth.Properties.Mask.ShowPlaceHolders = false;
+         this.txtStartMonth.Properties.Mask.UseMaskAsDisplayFormat = true;
+         this.txtStartMonth.Size = new System.Drawing.Size(100, 26);
+         this.txtStartMonth.TabIndex = 17;
+         this.txtStartMonth.TextMaskFormat = BaseGround.Widget.TextDateEdit.TextMaskFormatItem.IncludePrompt;
+         // 
+         // rgpType
+         // 
+         this.rgpType.EditValue = "fcm";
+         this.rgpType.Location = new System.Drawing.Point(137, 122);
+         this.rgpType.Name = "rgpType";
+         this.rgpType.Properties.Appearance.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(166)))), ((int)(((byte)(202)))), ((int)(((byte)(240)))));
+         this.rgpType.Properties.Appearance.Options.UseBackColor = true;
+         this.rgpType.Properties.Items.AddRange(new DevExpress.XtraEditors.Controls.RadioGroupItem[] {
+            new DevExpress.XtraEditors.Controls.RadioGroupItem("fcm", "依期貨商別"),
+            new DevExpress.XtraEditors.Controls.RadioGroupItem("product", "依商品別")});
+         this.rgpType.Size = new System.Drawing.Size(283, 40);
+         this.rgpType.TabIndex = 4;
+         // 
+         // labFcmNo
+         // 
+         this.labFcmNo.AutoSize = true;
+         this.labFcmNo.ForeColor = System.Drawing.Color.Black;
+         this.labFcmNo.Location = new System.Drawing.Point(64, 88);
+         this.labFcmNo.Name = "labFcmNo";
+         this.labFcmNo.Size = new System.Drawing.Size(71, 15);
+         this.labFcmNo.TabIndex = 16;
+         this.labFcmNo.Text = "期貨商：";
+         // 
+         // label1
+         // 
+         this.label1.AutoSize = true;
+         this.label1.Location = new System.Drawing.Point(432, 84);
+         this.label1.Name = "label1";
+         this.label1.Size = new System.Drawing.Size(16, 15);
+         this.label1.TabIndex = 14;
+         this.label1.Text = "~";
+         // 
+         // lbl1
+         // 
+         this.lbl1.AutoSize = true;
+         this.lbl1.Location = new System.Drawing.Point(243, 43);
+         this.lbl1.Name = "lbl1";
+         this.lbl1.Size = new System.Drawing.Size(16, 15);
+         this.lbl1.TabIndex = 11;
+         this.lbl1.Text = "~";
+         // 
+         // labDate
+         // 
+         this.labDate.AutoSize = true;
+         this.labDate.ForeColor = System.Drawing.Color.Black;
+         this.labDate.Location = new System.Drawing.Point(79, 47);
+         this.labDate.Name = "labDate";
+         this.labDate.Size = new System.Drawing.Size(55, 15);
+         this.labDate.TabIndex = 2;
+         this.labDate.Text = "月份：";
+         // 
+         // cbxFcmStartNo
+         // 
+         this.cbxFcmStartNo.Location = new System.Drawing.Point(137, 81);
+         this.cbxFcmStartNo.Name = "cbxFcmStartNo";
+         this.cbxFcmStartNo.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
+         this.cbxFcmStartNo.Size = new System.Drawing.Size(289, 26);
+         this.cbxFcmStartNo.TabIndex = 2;
+         // 
+         // cbxFcmEndNo
+         // 
+         this.cbxFcmEndNo.Location = new System.Drawing.Point(452, 81);
+         this.cbxFcmEndNo.Name = "cbxFcmEndNo";
+         this.cbxFcmEndNo.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
+         this.cbxFcmEndNo.Size = new System.Drawing.Size(289, 26);
+         this.cbxFcmEndNo.TabIndex = 3;
+         // 
+         // labMsg
+         // 
+         this.labMsg.AutoSize = true;
+         this.labMsg.ForeColor = System.Drawing.Color.Blue;
+         this.labMsg.Location = new System.Drawing.Point(12, 247);
+         this.labMsg.Name = "labMsg";
+         this.labMsg.Size = new System.Drawing.Size(169, 20);
+         this.labMsg.TabIndex = 18;
+         this.labMsg.Text = "訊息：資料轉出中........";
+         this.labMsg.Visible = false;
+         // 
+         // r_frame
+         // 
+         this.r_frame.Appearance.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(166)))), ((int)(((byte)(202)))), ((int)(((byte)(240)))));
+         this.r_frame.Appearance.Options.UseBackColor = true;
+         this.r_frame.BorderStyle = DevExpress.XtraEditors.Controls.BorderStyles.NoBorder;
+         this.r_frame.Controls.Add(this.panFilter);
+         this.r_frame.Controls.Add(this.labMsg);
+         this.r_frame.Location = new System.Drawing.Point(15, 15);
+         this.r_frame.Name = "r_frame";
+         this.r_frame.Size = new System.Drawing.Size(820, 273);
+         this.r_frame.TabIndex = 78;
+         // 
          // W55050
          // 
          this.Appearance.Options.UseFont = true;
@@ -200,15 +219,17 @@
          this.Name = "W55050";
          this.Text = "W55050";
          this.panParent.ResumeLayout(false);
-         this.panParent.PerformLayout();
          ((System.ComponentModel.ISupportInitialize)(this.ribbonControl)).EndInit();
          this.panFilter.ResumeLayout(false);
          this.panFilter.PerformLayout();
+         ((System.ComponentModel.ISupportInitialize)(this.txtEndMonth.Properties)).EndInit();
+         ((System.ComponentModel.ISupportInitialize)(this.txtStartMonth.Properties)).EndInit();
          ((System.ComponentModel.ISupportInitialize)(this.rgpType.Properties)).EndInit();
          ((System.ComponentModel.ISupportInitialize)(this.cbxFcmStartNo.Properties)).EndInit();
          ((System.ComponentModel.ISupportInitialize)(this.cbxFcmEndNo.Properties)).EndInit();
-         ((System.ComponentModel.ISupportInitialize)(this.txtStartMonth.Properties)).EndInit();
-         ((System.ComponentModel.ISupportInitialize)(this.txtEndMonth.Properties)).EndInit();
+         ((System.ComponentModel.ISupportInitialize)(this.r_frame)).EndInit();
+         this.r_frame.ResumeLayout(false);
+         this.r_frame.PerformLayout();
          this.ResumeLayout(false);
          this.PerformLayout();
 
@@ -227,5 +248,6 @@
         private System.Windows.Forms.Label labMsg;
       private BaseGround.Widget.TextDateEdit txtEndMonth;
       private BaseGround.Widget.TextDateEdit txtStartMonth;
+      private DevExpress.XtraEditors.PanelControl r_frame;
    }
 }
