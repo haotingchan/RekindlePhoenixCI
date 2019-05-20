1b83a524ac193b3ba7a10ff00255dd8e027bd357
john <john@kjsoft.com.tw>
2019-05-20 20:15:27 +0800

    [modify]500XX系列存取子

--- DataObjects/Dao/Together/SpecificDao/D500xx.cs
+++ DataObjects/Dao/Together/SpecificDao/D500xx.cs
@@ -4,6 +4,7 @@ using System.Data;
 using System.Data.Common;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace DataObjects.Dao.Together.SpecificDao
@@ -23,11 +24,70 @@ namespace DataObjects.Dao.Together.SpecificDao
       public string SumSubType { get; set; }
       public string DataType { get; set; }
       public string SortType { get; set; }
-      public string TableName { get; set; }
+      public string TableName { get; set; } = "AMM0";
       public string LogText { get; set; }
       public string TxnID { get; set; }
       public string TimeNow { get; set; }
       public string Filename { get; set; }
-      public DataTable Data { get; set; }
+      public string GbGroup { get; set; }
+
+      public string ConditionWhereSyntax()
+      {
+         StringBuilder iswhere = new StringBuilder("");
+         /* 日期起迄 */
+         if (!string.IsNullOrEmpty(Sdate)) {
+            iswhere.Append($@" and {TableName}_YMD >='{CheckValue(Sdate)}' ");
+         }
+         if (!string.IsNullOrEmpty(Edate)) {
+            iswhere.Append($@" and {TableName}_YMD <='{CheckValue(Edate)}' ");
+         }
+         /* 期貨商代號起迄 */
+         if (!string.IsNullOrEmpty(Sbrkno)) {
+            iswhere.Append($@" and {TableName}_BRK_NO >='{CheckValue(Sbrkno)}' ");
+         }
+         if (!string.IsNullOrEmpty(Ebrkno)) {
+            iswhere.Append($@" and {TableName}_BRK_NO <='{CheckValue(Ebrkno)}' ");
+         }
+
+         /*******************
+         Where條件
+         *******************/
+         /* 商品群組 */
+         if (!GbGroup.Equals("rb_gall")) {
+            if (!string.IsNullOrEmpty(ProdCategory)) {
+               iswhere.Append($@" and {TableName}_PARAM_KEY ='{CheckValue(ProdCategory)}' ");
+            }
+            /* 個股商品 */
+            if (!GbGroup.Equals("rb_gparam")) {
+               if (!string.IsNullOrEmpty(ProdKindIdSto)) {
+                  iswhere.Append($@" and {TableName}_KIND_ID2 ='{CheckValue(ProdKindIdSto)}' ");
+               }
+               /* 商品 */
+               if (!GbGroup.Equals("rb_gkind2")) {
+                  if (!string.IsNullOrEmpty(ProdKindId)) {
+                     iswhere.Append($@" and {TableName}_KIND_ID ='{CheckValue(ProdKindId)}' ");
+                  }
+               }//rb_gkind2.checked = False
+            }//rb_gparam.checked = False
+         }//rb_gall.checked = False
+         return iswhere.ToString();
+      }
+
+      private string CheckValue(string str)
+      {
+
+         RegexOptions opt = new RegexOptions();
+         opt = RegexOptions.IgnorePatternWhitespace;
+         Regex reg = new Regex("[A-Za-z0-9]+", opt);
+
+         if (!string.IsNullOrEmpty(str)) {
+            if (!reg.Match(str).Success) {
+               throw new Exception("Value Error");
+            }
+         }
+
+         return str;
+      }
+
    }
 }
