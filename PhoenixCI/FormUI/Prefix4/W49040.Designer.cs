7b57bff4e65e09785ca2e6c18414472a31733ef6
winni <winni@kjsoft.com.tw>
2019-05-21 15:40:00 +0800

    [Modify] MGT4 / 20320 / 20330 / 20340 / 49040

--- PhoenixCI/FormUI/Prefix4/W49040.Designer.cs
+++ PhoenixCI/FormUI/Prefix4/W49040.Designer.cs
@@ -26,16 +26,6 @@
          this.panelControl2 = new DevExpress.XtraEditors.PanelControl();
          this.gcMain = new DevExpress.XtraGrid.GridControl();
          this.gvMain = new DevExpress.XtraGrid.Views.Grid.GridView();
-         this.MGT4_KIND_ID = new DevExpress.XtraGrid.Columns.GridColumn();
-         this.MGT4_TYPE = new DevExpress.XtraGrid.Columns.GridColumn();
-         this.MGT4_CUR_M_MULTI = new DevExpress.XtraGrid.Columns.GridColumn();
-         this.MGT4_CUR_I_MULTI = new DevExpress.XtraGrid.Columns.GridColumn();
-         this.MGT4_CUR_DIGITAL = new DevExpress.XtraGrid.Columns.GridColumn();
-         this.MGT4_M_MULTI = new DevExpress.XtraGrid.Columns.GridColumn();
-         this.MGT4_I_MULTI = new DevExpress.XtraGrid.Columns.GridColumn();
-         this.MGT4_DIGITAL = new DevExpress.XtraGrid.Columns.GridColumn();
-         this.MGT4_M_DIGITAL = new DevExpress.XtraGrid.Columns.GridColumn();
-         this.Is_NewRow = new DevExpress.XtraGrid.Columns.GridColumn();
          this.panParent.SuspendLayout();
          ((System.ComponentModel.ISupportInitialize)(this.ribbonControl)).BeginInit();
          ((System.ComponentModel.ISupportInitialize)(this.panelControl2)).BeginInit();
@@ -76,17 +66,6 @@
          // 
          // gvMain
          // 
-         this.gvMain.Columns.AddRange(new DevExpress.XtraGrid.Columns.GridColumn[] {
-            this.MGT4_KIND_ID,
-            this.MGT4_TYPE,
-            this.MGT4_CUR_M_MULTI,
-            this.MGT4_CUR_I_MULTI,
-            this.MGT4_CUR_DIGITAL,
-            this.MGT4_M_MULTI,
-            this.MGT4_I_MULTI,
-            this.MGT4_DIGITAL,
-            this.MGT4_M_DIGITAL,
-            this.Is_NewRow});
          this.gvMain.GridControl = this.gcMain;
          this.gvMain.Name = "gvMain";
          this.gvMain.OptionsView.ColumnAutoWidth = false;
@@ -94,119 +73,11 @@
          this.gvMain.RowCellStyle += new DevExpress.XtraGrid.Views.Grid.RowCellStyleEventHandler(this.gvMain_RowCellStyle);
          this.gvMain.ShowingEditor += new System.ComponentModel.CancelEventHandler(this.gvMain_ShowingEditor);
          // 
-         // MGT4_KIND_ID
-         // 
-         this.MGT4_KIND_ID.AppearanceHeader.BackColor = System.Drawing.Color.Yellow;
-         this.MGT4_KIND_ID.AppearanceHeader.Options.UseBackColor = true;
-         this.MGT4_KIND_ID.AppearanceHeader.Options.UseTextOptions = true;
-         this.MGT4_KIND_ID.AppearanceHeader.TextOptions.HAlignment = DevExpress.Utils.HorzAlignment.Center;
-         this.MGT4_KIND_ID.Caption = "商品";
-         this.MGT4_KIND_ID.FieldName = "MGT4_KIND_ID";
-         this.MGT4_KIND_ID.Name = "MGT4_KIND_ID";
-         this.MGT4_KIND_ID.Visible = true;
-         this.MGT4_KIND_ID.VisibleIndex = 0;
-         // 
-         // MGT4_TYPE
-         // 
-         this.MGT4_TYPE.AppearanceHeader.BackColor = System.Drawing.Color.Yellow;
-         this.MGT4_TYPE.AppearanceHeader.Options.UseBackColor = true;
-         this.MGT4_TYPE.AppearanceHeader.Options.UseTextOptions = true;
-         this.MGT4_TYPE.AppearanceHeader.TextOptions.HAlignment = DevExpress.Utils.HorzAlignment.Center;
-         this.MGT4_TYPE.Caption = "類別";
-         this.MGT4_TYPE.FieldName = "MGT4_TYPE";
-         this.MGT4_TYPE.Name = "MGT4_TYPE";
-         this.MGT4_TYPE.Visible = true;
-         this.MGT4_TYPE.VisibleIndex = 1;
-         this.MGT4_TYPE.Width = 200;
-         // 
-         // MGT4_CUR_M_MULTI
-         // 
-         this.MGT4_CUR_M_MULTI.AppearanceHeader.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
-         this.MGT4_CUR_M_MULTI.AppearanceHeader.Options.UseBackColor = true;
-         this.MGT4_CUR_M_MULTI.AppearanceHeader.Options.UseTextOptions = true;
-         this.MGT4_CUR_M_MULTI.AppearanceHeader.TextOptions.HAlignment = DevExpress.Utils.HorzAlignment.Center;
-         this.MGT4_CUR_M_MULTI.Caption = "現行維持比率";
-         this.MGT4_CUR_M_MULTI.FieldName = "MGT4_CUR_M_MULTI";
-         this.MGT4_CUR_M_MULTI.Name = "MGT4_CUR_M_MULTI";
-         this.MGT4_CUR_M_MULTI.Visible = true;
-         this.MGT4_CUR_M_MULTI.VisibleIndex = 2;
-         this.MGT4_CUR_M_MULTI.Width = 145;
-         // 
-         // MGT4_CUR_I_MULTI
-         // 
-         this.MGT4_CUR_I_MULTI.AppearanceHeader.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
-         this.MGT4_CUR_I_MULTI.AppearanceHeader.Options.UseBackColor = true;
-         this.MGT4_CUR_I_MULTI.AppearanceHeader.Options.UseTextOptions = true;
-         this.MGT4_CUR_I_MULTI.AppearanceHeader.TextOptions.HAlignment = DevExpress.Utils.HorzAlignment.Center;
-         this.MGT4_CUR_I_MULTI.Caption = "現行原始比率";
-         this.MGT4_CUR_I_MULTI.FieldName = "MGT4_CUR_I_MULTI";
-         this.MGT4_CUR_I_MULTI.Name = "MGT4_CUR_I_MULTI";
-         this.MGT4_CUR_I_MULTI.Visible = true;
-         this.MGT4_CUR_I_MULTI.VisibleIndex = 3;
-         this.MGT4_CUR_I_MULTI.Width = 120;
-         // 
-         // MGT4_CUR_DIGITAL
-         // 
-         this.MGT4_CUR_DIGITAL.AppearanceHeader.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
-         this.MGT4_CUR_DIGITAL.AppearanceHeader.Options.UseBackColor = true;
-         this.MGT4_CUR_DIGITAL.AppearanceHeader.Options.UseTextOptions = true;
-         this.MGT4_CUR_DIGITAL.AppearanceHeader.TextOptions.HAlignment = DevExpress.Utils.HorzAlignment.Center;
-         this.MGT4_CUR_DIGITAL.Caption = "現行進位數";
-         this.MGT4_CUR_DIGITAL.FieldName = "MGT4_CUR_DIGITAL";
-         this.MGT4_CUR_DIGITAL.Name = "MGT4_CUR_DIGITAL";
-         this.MGT4_CUR_DIGITAL.Visible = true;
-         this.MGT4_CUR_DIGITAL.VisibleIndex = 4;
-         this.MGT4_CUR_DIGITAL.Width = 145;
-         // 
-         // MGT4_M_MULTI
-         // 
-         this.MGT4_M_MULTI.AppearanceHeader.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
-         this.MGT4_M_MULTI.AppearanceHeader.Options.UseBackColor = true;
-         this.MGT4_M_MULTI.Caption = "本日維持比率";
-         this.MGT4_M_MULTI.FieldName = "MGT4_M_MULTI";
-         this.MGT4_M_MULTI.Name = "MGT4_M_MULTI";
-         this.MGT4_M_MULTI.Visible = true;
-         this.MGT4_M_MULTI.VisibleIndex = 5;
-         // 
-         // MGT4_I_MULTI
-         // 
-         this.MGT4_I_MULTI.AppearanceHeader.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
-         this.MGT4_I_MULTI.AppearanceHeader.Options.UseBackColor = true;
-         this.MGT4_I_MULTI.Caption = "本日原始比率";
-         this.MGT4_I_MULTI.FieldName = "MGT4_I_MULTI";
-         this.MGT4_I_MULTI.Name = "MGT4_I_MULTI";
-         this.MGT4_I_MULTI.Visible = true;
-         this.MGT4_I_MULTI.VisibleIndex = 6;
-         // 
-         // MGT4_DIGITAL
-         // 
-         this.MGT4_DIGITAL.AppearanceHeader.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
-         this.MGT4_DIGITAL.AppearanceHeader.Options.UseBackColor = true;
-         this.MGT4_DIGITAL.Caption = "本日進位數";
-         this.MGT4_DIGITAL.FieldName = "MGT4_DIGITAL";
-         this.MGT4_DIGITAL.Name = "MGT4_DIGITAL";
-         this.MGT4_DIGITAL.Visible = true;
-         this.MGT4_DIGITAL.VisibleIndex = 7;
-         // 
-         // MGT4_M_DIGITAL
-         // 
-         this.MGT4_M_DIGITAL.AppearanceHeader.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
-         this.MGT4_M_DIGITAL.AppearanceHeader.Options.UseBackColor = true;
-         this.MGT4_M_DIGITAL.Caption = "本日結算進位數";
-         this.MGT4_M_DIGITAL.FieldName = "MGT4_M_DIGITAL";
-         this.MGT4_M_DIGITAL.Name = "MGT4_M_DIGITAL";
-         this.MGT4_M_DIGITAL.Visible = true;
-         this.MGT4_M_DIGITAL.VisibleIndex = 8;
-         // 
-         // Is_NewRow
-         // 
-         this.Is_NewRow.Caption = "Is_NewRow";
-         this.Is_NewRow.FieldName = "Is_NewRow";
-         this.Is_NewRow.Name = "Is_NewRow";
-         // 
          // W49040
          // 
+         this.Appearance.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(31)))), ((int)(((byte)(53)))));
          this.Appearance.Options.UseFont = true;
+         this.Appearance.Options.UseForeColor = true;
          this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
          this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
          this.ClientSize = new System.Drawing.Size(1035, 647);
@@ -230,15 +101,5 @@
         private DevExpress.XtraEditors.PanelControl panelControl2;
         private DevExpress.XtraGrid.GridControl gcMain;
         private DevExpress.XtraGrid.Views.Grid.GridView gvMain;
-        private DevExpress.XtraGrid.Columns.GridColumn MGT4_KIND_ID;
-        private DevExpress.XtraGrid.Columns.GridColumn MGT4_TYPE;
-        private DevExpress.XtraGrid.Columns.GridColumn MGT4_CUR_M_MULTI;
-        private DevExpress.XtraGrid.Columns.GridColumn MGT4_CUR_I_MULTI;
-        private DevExpress.XtraGrid.Columns.GridColumn MGT4_CUR_DIGITAL;
-        private DevExpress.XtraGrid.Columns.GridColumn MGT4_M_MULTI;
-        private DevExpress.XtraGrid.Columns.GridColumn MGT4_I_MULTI;
-        private DevExpress.XtraGrid.Columns.GridColumn Is_NewRow;
-      private DevExpress.XtraGrid.Columns.GridColumn MGT4_DIGITAL;
-      private DevExpress.XtraGrid.Columns.GridColumn MGT4_M_DIGITAL;
    }
 }
