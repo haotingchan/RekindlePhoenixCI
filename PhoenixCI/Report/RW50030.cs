25df37bb6632385378c0d44f97904fa5342a60b2
john <john@kjsoft.com.tw>
2019-06-03 10:14:11 +0800

    [modify]調整報表相關寬度大小、研究PB加總邏輯(PB的加總有些並不是真的加總)

--- PhoenixCI/Report/RW50030.cs
+++ PhoenixCI/Report/RW50030.cs
@@ -24,10 +24,24 @@ namespace PhoenixCI.Report
       {
          switch (sorttype) {
             case "F":
-               this.xrTableRow4.Cells.Remove(this.cp_prod2);
-               this.xrTableRow1.Cells.Remove(this.cp_prod_id2);
-               this.cp_prod1.WidthF = 68f;
-               this.cp_prod_id.WidthF = 68f;
+               this.cp_prod2.WidthF = 0f;
+               this.cp_prod_id2.WidthF = 0f;
+
+               //期貨商代號
+               this.xrTableCell17.WidthF = 62.86f;
+               this.amm0_brk_no.WidthF = 62.86f;
+               //期貨商名稱
+               this.xrTableCell18.WidthF = 115f;
+               this.brk_abbr_name.WidthF = 115f;
+               //投資人帳號
+               this.xrTableCell19.WidthF = 53f;
+               this.amm0_acc_no.WidthF = 53f;
+               //商品名稱
+               this.cp_prod1.WidthF = 80f;
+               this.cp_prod_id.WidthF = 80f;
+               //一般委託成交量
+               this.xrTableCell21.WidthF = 59.48f;
+               this.amm0_om_qnty.WidthF = 59.48f;
                //列印按"商品"才有小計 其他時候隱藏
                xrTableRow2.HeightF = 0;
                xrTableRow2.Visible = false;
@@ -38,10 +52,24 @@ namespace PhoenixCI.Report
 
                break;
             case "P":
-               this.xrTableRow4.Cells.Remove(this.cp_prod1);
-               this.xrTableRow1.Cells.Remove(this.cp_prod_id);
-               this.cp_prod2.WidthF = 68f;
-               this.cp_prod_id2.WidthF = 68f;
+               this.cp_prod1.WidthF = 0f;
+               this.cp_prod_id.WidthF = 0f;
+               
+               //期貨商代號
+               this.xrTableCell17.WidthF = 62.86f;
+               this.amm0_brk_no.WidthF = 62.86f;
+               //期貨商名稱
+               this.xrTableCell18.WidthF = 115f;
+               this.brk_abbr_name.WidthF = 115f;
+               //投資人帳號
+               this.xrTableCell19.WidthF = 53f;
+               this.amm0_acc_no.WidthF = 53f;
+               //一般委託成交量
+               this.xrTableCell21.WidthF = 59.48f;
+               this.amm0_om_qnty.WidthF = 59.48f;
+               //商品名稱
+               this.cp_prod2.WidthF = 80f;
+               this.cp_prod_id2.WidthF = 80f;
                break;
             default:
                break;
