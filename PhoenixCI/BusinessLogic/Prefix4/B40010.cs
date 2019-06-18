ab828c4c3fa228650c6bd93d93a2ff3fe77297fd
john <john@kjsoft.com.tw>
2019-06-18 15:33:44 +0800

    [modify]EWMA計算 sql修改

--- PhoenixCI/BusinessLogic/Prefix4/B40010.cs
+++ PhoenixCI/BusinessLogic/Prefix4/B40010.cs
@@ -1,14 +1,10 @@
-﻿using BaseGround.Shared;
+﻿using BusinessObjects.Enums;
 using Common;
 using DataObjects.Dao.Together.SpecificDao;
 using DevExpress.Spreadsheet;
 using System;
-using System.Collections.Generic;
 using System.Data;
 using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Threading.Tasks;
 /// <summary>
 /// john,20190611
 /// </summary>
@@ -50,10 +46,15 @@ namespace PhoenixCI.BusinessLogic.Prefix4
          return dt;
       }
 
-      public void MGR2Save(DataTable MGR2_SMA)
+      public void MGR2SaveToDB(DataTable MGR2_SMA, string oswGrp)
       {
+         DateTime emdate = _emDateText.AsDateTime("yyyy/MM/dd");
          //儲存至DB
-         dao40010.UpdateMGR2_SMA(MGR2_SMA);
+         ResultStatus resultStatus = dao40010.UpdateMGR2_SMA(MGR2_SMA).Status;
+         //呼叫opt.sp_O_gen_H_MG1_3M
+         if (resultStatus == ResultStatus.Success) {
+            dao40010.SP_O_gen_H_MG1_3M(emdate, oswGrp);
+         }
       }
 
       /// <summary>
@@ -75,11 +76,11 @@ namespace PhoenixCI.BusinessLogic.Prefix4
             //確認有無資料
             DataTable RowData = dao40010.ListRowDataSheet(lsDate, KindID);
             if (RowData.Rows.Count <= 0) {
-               File.Delete(FilePath);
                return null;
             }
 
             worksheet.Cells["A1"].SetValue(RowData.Rows[0]["MG1_PROD_SUBTYPE"]);
+            worksheet.Cells["B1"].SetValue(RowData.Rows[0]["MGP1_PROD_TYPE"]);
             worksheet.Cells["C1"].SetValue(RowData.Rows[0]["KIND_ID"]);
             worksheet.Cells["D1"].SetValue(RowData.Rows[0]["MG1_PARAM_KEY"]);
             worksheet.Cells["E1"].SetValue(RowData.Rows[0]["MG1_M_KIND_ID"]);
@@ -89,29 +90,20 @@ namespace PhoenixCI.BusinessLogic.Prefix4
             worksheet.Cells["O3"].SetValue(RowData.Rows[0]["MG1_XXX"]);
 
             //2.寫入40010template計算EWMA
-            //共15欄位 取前7欄位
+            //共16欄位 取前7欄位
             for (int k = 7; k < RowData.Columns.Count; k++) {
-               RowData.Columns.Remove(RowData.Columns[7].ColumnName);//刪除後面8欄
+               RowData.Columns.Remove(RowData.Columns[7].ColumnName);//刪除後面9欄
             }
             worksheet.Import(RowData, false, 2, 0);
             worksheet.ScrollTo(0, 0);
             //save
             workbook.SaveDocument(FilePath);
 
-            //3.RowData寫回DB
-            //確認有無資料 沒有則新增一行
-            DataTable MGR2_SMA = dao40010.ListMGR2_SMA(lsDate, KindID);
-            
-            if (MGR2_SMA.Rows.Count <= 0) {
-               row = MGR2_SMA.NewRow();
-               MGR2_SMA.Rows.Add(row);
-            }
-            else {
-               row = MGR2_SMA.Rows[0];
-            }
-            string f6 = "F6";
-            string f4 = "F4";
-            string f2 = "F2";
+            //3.return datarow
+            DataTable MGR2_SMA = MGR2DataClone();
+
+            row = MGR2_SMA.NewRow();
+            MGR2_SMA.Rows.Add(row);
 
             //模型(S:SMA/M:MAXV/E:EWMA)
             row["MGR2_MODEL_TYPE"] = "E";
@@ -120,25 +112,25 @@ namespace PhoenixCI.BusinessLogic.Prefix4
             //股票代號
             row["MGR2_M_KIND_ID"] = worksheet.Cells["E1"].Value;
             //B開參/最低價 (A-B)
-            row["MGR2_PRICE1"] = worksheet.Cells["E3"].Value.NumericValue.ToString(f2).AsDecimal();
+            row["MGR2_PRICE1"] = worksheet.Cells["E3"].Value.NumericValue;
             //A收盤價/最高價/開參 (A-B)
-            row["MGR2_PRICE2"] = worksheet.Cells["D3"].Value.NumericValue.ToString(f2).AsDecimal();
+            row["MGR2_PRICE2"] = worksheet.Cells["D3"].Value.NumericValue;
             //每日報酬率
-            row["MGR2_RETURN_RATE"] = worksheet.Cells["G3"].Value.NumericValue.ToString(f2).AsDecimal();
+            row["MGR2_RETURN_RATE"] = worksheet.Cells["G3"].Value.NumericValue;
             //30日風險價格係數
-            row["MGR2_30_RATE"] = worksheet.Cells["H3"].Value.NumericValue.ToString(f2).AsDecimal();
+            row["MGR2_30_RATE"] = worksheet.Cells["H3"].Value.NumericValue;
             //60日風險價格係數
-            row["MGR2_60_RATE"] = worksheet.Cells["I3"].Value.NumericValue.ToString(f2).AsDecimal();
+            row["MGR2_60_RATE"] = worksheet.Cells["I3"].Value.NumericValue;
             //90日風險價格係數
-            row["MGR2_90_RATE"] = worksheet.Cells["J3"].Value.NumericValue.ToString(f2).AsDecimal();
+            row["MGR2_90_RATE"] = worksheet.Cells["J3"].Value.NumericValue;
             //180日風險價格係數
-            row["MGR2_180_RATE"] = worksheet.Cells["K3"].Value.NumericValue.ToString(f2).AsDecimal();
+            row["MGR2_180_RATE"] = worksheet.Cells["K3"].Value.NumericValue;
             //2500日風險價格係數
-            row["MGR2_2500_RATE"] = worksheet.Cells["L3"].Value.NumericValue.ToString(f2).AsDecimal();
+            row["MGR2_2500_RATE"] = worksheet.Cells["L3"].Value.NumericValue;
             //風險價格係數最大值
-            row["MGR2_MIN_RATE"] = worksheet.Cells["N1"].Value.NumericValue.ToString(f2).AsDecimal();
+            row["MGR2_MIN_RATE"] = worksheet.Cells["N1"].Value.NumericValue;
             //每日風險價格係數
-            row["MGR2_DAY_RATE"] = worksheet.Cells["N3"].Value.NumericValue.ToString(f2).AsDecimal();
+            row["MGR2_DAY_RATE"] = worksheet.Cells["N3"].Value.NumericValue;
             /*資料計算狀態
               N  正常                
               X  不計算*/
@@ -150,14 +142,14 @@ namespace PhoenixCI.BusinessLogic.Prefix4
             //契約對照瑪
             row["MGR2_PARAM_KEY"] = worksheet.Cells["D1"].Value;
             //計算的風險價格係數
-            row["MGR2_CP_RATE"] = worksheet.Cells["M3"].Value.NumericValue.ToString(f2).AsDecimal();
+            row["MGR2_CP_RATE"] = worksheet.Cells["M3"].Value.NumericValue;
             //計算的風險價格係數(涵蓋1日)
-            row["MGR2_1DAY_CP_RATE"] = worksheet.Cells["M1"].Value.NumericValue.ToString(f2).AsDecimal();
+            row["MGR2_1DAY_CP_RATE"] = worksheet.Cells["M1"].Value.NumericValue;
             //轉檔時間
             row["MGR2_W_TIME"] = DateTime.Now;
             //收盤群組
             row["MGR2_OSW_GRP"] = worksheet.Cells["F1"].Value;
-            
+
          }
          catch (Exception ex) {
 #if DEBUG
