039fd7d9339a8fa7ec89b41ad68e142e9bae1555
samdylo <samdy800322@gmail.com>
2019-07-05 17:55:38 +0800

    55010報表內容修改

--- DataObjects/Dao/Together/SpecificDao/D55010.cs
+++ DataObjects/Dao/Together/SpecificDao/D55010.cs
@@ -16,29 +16,34 @@ namespace DataObjects.Dao.Together.SpecificDao {
                "@as_eym",as_eym
             };
 
-         string sql = @"
-select 
-   feetrd_fcm_no,
-   feetrd_acc_no,  
-   nvl(abrk_name,'') as brk_abbr_name,
-   feetrd_kind_id as sort_kind_id,
-   case when feetrd_kind_id = 'TXW' then ' └─'||trim(feetrd_kind_id) else feetrd_kind_id end as feetrd_kind_id,   
-   sum(feetrd_disc_qnty) as feetrd_disc_qnty,   
-   max(1 - feetrd_rec_rate) as disc_rate,   
-   sum(feetrd_mk_disc_amt + feetrd_oth_disc_amt) as disc_amt  --,FEETRD_SESSION 
-from ci.feetrd,ci.abrk 
-where feetrd_ym >= :as_sym 
-and feetrd_ym <= :as_eym 
-and feetrd_fcm_kind = '3'
-and (feetrd_param_key = feetrd_kind_id or feetrd_kind_id = 'TXW')
-and feetrd_fcm_no = abrk_no(+)
-group by 
-    feetrd_fcm_no,
-    feetrd_acc_no,  
-    abrk_name,
-    feetrd_kind_id
-order by feetrd_fcm_no ,feetrd_acc_no , sort_kind_id
-";
+            string sql = @"
+                                select 
+                                   feetrd_fcm_no,
+                                   feetrd_acc_no,  
+                                   nvl(abrk_name,'') as brk_abbr_name,
+                                   feetrd_kind_id as sort_kind_id,
+                                   case when feetrd_kind_id = 'TXW' then ' └─'||trim(feetrd_kind_id) else feetrd_kind_id end as feetrd_kind_id,   
+                                   sum(case when (feetrd_kind_id in ('TXO','TXW') and feetrd_session =0 ) or  feetrd_session = 1 then feetrd_disc_qnty else 0 end) as feetrd_disc_qnty,   
+                                   sum(case when (feetrd_kind_id in ('TXO','TXW') and feetrd_session =0 ) or feetrd_session = 1 then 1 - feetrd_rec_rate else 0 end) as disc_rate,   
+                                   sum(case when (feetrd_kind_id in ('TXO','TXW') and feetrd_session =0 ) or feetrd_session = 1 then feetrd_mk_disc_amt + feetrd_oth_disc_amt else 0 end) as disc_amt,
+                                   sum(case when feetrd_session = 2 then feetrd_disc_qnty else 0 end) as feetrd_disc_qnty_ah,   
+                                   sum(case when feetrd_session = 2 then 1 - feetrd_rec_rate else 0 end) as disc_rate_ah,   
+                                   sum(case when feetrd_session = 2 then feetrd_mk_disc_amt + feetrd_oth_disc_amt else 0 end) as disc_amt_ah,
+                                   sum(feetrd_disc_qnty) as feetrd_disc_qnty_sum,    
+                                   sum(feetrd_mk_disc_amt + feetrd_oth_disc_amt) as disc_amt_sum
+                                from ci.feetrd,ci.abrk 
+                                where feetrd_ym >= :as_sym 
+                                and feetrd_ym <=  :as_eym 
+                                and feetrd_fcm_kind = '3'
+                                and (feetrd_param_key = feetrd_kind_id or feetrd_kind_id = 'TXW')
+                                and feetrd_fcm_no = abrk_no(+)
+                                group by 
+                                    feetrd_fcm_no,
+                                    feetrd_acc_no,  
+                                    abrk_name,
+                                    feetrd_kind_id
+                                order by feetrd_fcm_no ,feetrd_acc_no , sort_kind_id            
+                                ";
 
          DataTable dtResult = db.GetDataTable(sql , parms);
 
@@ -60,9 +65,14 @@ select
    nvl(abrk_name,'') as brk_abbr_name,
    feetrd_kind_id as sort_kind_id,
    case when feetrd_kind_id = 'TXW' then ' └─'||trim(feetrd_kind_id) else feetrd_kind_id end as feetrd_kind_id,   
-   sum(feetrd_disc_qnty) as feetrd_disc_qnty,   
-   max(1 - feetrd_rec_rate) as disc_rate,   
-   sum(feetrd_mk_disc_amt + feetrd_oth_disc_amt) as disc_amt  --,FEETRD_SESSION 
+   sum(case when (feetrd_kind_id in ('TXO','TXW') and feetrd_session =0 ) or  feetrd_session = 1 then feetrd_disc_qnty else 0 end) as feetrd_disc_qnty,   
+   sum(case when (feetrd_kind_id in ('TXO','TXW') and feetrd_session =0 ) or feetrd_session = 1 then 1 - feetrd_rec_rate else 0 end) as disc_rate,   
+   sum(case when (feetrd_kind_id in ('TXO','TXW') and feetrd_session =0 ) or feetrd_session = 1 then feetrd_mk_disc_amt + feetrd_oth_disc_amt else 0 end) as disc_amt,
+   sum(case when feetrd_session = 2 then feetrd_disc_qnty else 0 end) as feetrd_disc_qnty_ah,   
+   sum(case when feetrd_session = 2 then 1 - feetrd_rec_rate else 0 end) as disc_rate_ah,   
+   sum(case when feetrd_session = 2 then feetrd_mk_disc_amt + feetrd_oth_disc_amt else 0 end) as disc_amt_ah,
+   sum(feetrd_disc_qnty) as feetrd_disc_qnty_sum,    
+   sum(feetrd_mk_disc_amt + feetrd_oth_disc_amt) as disc_amt_sum
 from 
    ci.feetrd,
    ci.abrk 
